8f000a86bf4701a43bf1243f1c45c91d97e21cf5
YEEUNco <121320706+YEEUNco@users.noreply.github.com>
2023-07-17 01:00:04 +0900

    [Bronze IV] Title: 별 찍기 - 3, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2440. 별 찍기 － 3/별 찍기 － 3.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int num;
+    cin >> num;
+    for(int i=0; i<num; i++){
+        for(int k=0; k<num-i; k++)
+            cout << "*";
+        cout <<"\n";
+    }
+}
